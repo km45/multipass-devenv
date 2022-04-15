8bddeb927c4bd3b723267da9dee123d8059e6e4b
Kamei Shogo <km45@users.noreply.github.com>
2022-04-15 22:36:15 +0900

    Install Ansible using cloud-init

--- test.sh
+++ test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -eux
 
+function test_ansible() {
+    type ansible-playbook
+}
+
 function test_zip() {
     type unzip
     type zip
@@ -8,6 +12,7 @@ function test_zip() {
 }
 
 function main() {
+    test_ansible
     test_zip
 }
 
